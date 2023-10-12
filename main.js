f2498688c7a30c3c5fa24e850b22c905cd0257d4
IsaiahMar <im7417615@gmail.com>
2023-10-12 09:32:52 -0400

    main

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-function solve(){
-    let val = document.getElementById('num')
-    val.value += val
+var operators = document.getElementsByClassName('operator')
+var display = document.getElementsByClassName('display')
+var button = document.getElementsByTagName('button')
+var result = ""
+
+// function calculate(oper){
+//     if(oper == "=" && result != ""){
+//        result = Function(result.replace("%", "/100"))
+//     }
+//     else if(oper == "AC"){
+//         result = ""
+//     }
+//     else if(oper == "Del"){
+//         result = toString().slice(0, -1)
+//     }
+//     else{
+//         if(result == ""){
+//             result += oper
+//         }
+//     }
+//     display.value = result;
+// }
+// calculate()
+function reset(){  
+    document.getElementsByClassName("buttons").reset();  
+  }   
+
+function Result(op, calc){
+    let num1 = document.getElementsByTagName("button")
+    let num2 = document.getElementsByTagName("button")
+    
+    var result = eval(num1 + op + num2)
+    document.getElementById(calc).value = result;
+    return result;
 }
-function clear(){
-    let num1 = document.getElementById('num').value
-    let num2 = Function(num1)
-    document.getElementById('num').value = num2
-}
-function Clear() {
-    var inp = document.getElementById('num');
-    inp.value = '';
- }
- function Back() {
-    var ev = document.getElementById('num');
-    ev.value = ev.value.slice(0,-1);
- }
