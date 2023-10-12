f71cb782581636510bc541e1a033a419e06e5f47
IsaiahMar <im7417615@gmail.com>
2023-10-12 15:16:19 -0400

    main

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
-var operators = document.getElementsByClassName('operator')
-var display = document.getElementsByClassName('display')
-var button = document.getElementsByTagName('button')
-var result = ""
+let display = document.querySelector(".display");
+let buttons1 = document.querySelectorAll("button");
+let operators = ["%", "*", "/", "-", "+", "="];
+let output = "";
+let checked;
 
-// function calculate(oper){
-//     if(oper == "=" && result != ""){
-//        result = Function(result.replace("%", "/100"))
-//     }
-//     else if(oper == "AC"){
-//         result = ""
-//     }
-//     else if(oper == "Del"){
-//         result = toString().slice(0, -1)
-//     }
-//     else{
-//         if(result == ""){
-//             result += oper
-//         }
-//     }
-//     display.value = result;
-// }
-// calculate()
-function reset(){  
-    document.getElementsByClassName("buttons").reset();  
-  }   
+function calculate(btnValue){
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+  
+    output = Function(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+   
+    output = output.toString().slice(0, -1);
+  } else {
+   
+    if (output === "" && operators.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
 
-function Result(op, calc){
-    let num1 = document.getElementsByTagName("button")
-    let num2 = document.getElementsByTagName("button")
-    
-    var result = eval(num1 + op + num2)
-    document.getElementById(calc).value = result;
-    return result;
+function buttons(button){
+    forEach((button) => {
+        button.addEventListener("click", (e) => calculate(e.target.dataset.value))
+            })
 }
