839da24bdc1c8233a6360e2affd123374ceb1e0a
IsaiahMar <im7417615@gmail.com>
2023-10-13 08:44:36 -0400

    main

--- main.js
+++ main.js
@@ -2,28 +2,31 @@ let display = document.querySelector(".display");
 let buttons1 = document.querySelectorAll("button");
 let operators = ["%", "*", "/", "-", "+", "="];
 let output = "";
-let checked;
+
 
 function calculate(btnValue){
   display.focus();
   if (btnValue === "=" && output !== "") {
   
-    output = Function(output.replace("%", "/100"));
-  } else if (btnValue === "AC") {
+    output = eval(output.replace("%", "/100"));
+  } 
+  
+  else if (btnValue === "AC") {
     output = "";
-  } else if (btnValue === "DEL") {
+  } 
+  
+  else if (btnValue === "DEL") {
    
     output = output.toString().slice(0, -1);
-  } else {
+  } 
+   else {
    
-    if (output === "" && operators.includes(btnValue)) return;
+if (output === "" && operators.includes(btnValue)) return;
     output += btnValue;
   }
   display.value = output;
 };
 
-function buttons(button){
-    forEach((button) => {
-        button.addEventListener("click", (e) => calculate(e.target.dataset.value))
-            })
-}
+buttons1.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
