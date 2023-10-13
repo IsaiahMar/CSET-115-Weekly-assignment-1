ff63b0f71b25ad393dc9194963923405788f9478
flight04 <xshefcyk4633@stevenscollege.edu>
2023-10-13 09:35:46 -0400

    added js

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll("button");
-const specialChars = ["%", "*", "/", "-", "+", "="];
+let display = document.querySelector(".display");
+let buttons1 = document.querySelectorAll("button");
+let operators = ["%", "*", "/", "-", "+", "="];
 let output = "";
-const calculate = (btnValue) => {
+
+
+function calculate(btnValue){
   display.focus();
   if (btnValue === "=" && output !== "") {
+  
     output = eval(output.replace("%", "/100"));
-  } else if (btnValue === "AC") {
+  } 
+  
+  else if (btnValue === "AC") {
     output = "";
-  } else if (btnValue === "DEL") {
+  } 
+  
+  else if (btnValue === "DEL") {
+   
     output = output.toString().slice(0, -1);
-  } else {
-    if (output === "" && specialChars.includes(btnValue)) return;
+  } 
+   else {
+   
+if (output === "" && operators.includes(btnValue)) return;
     output += btnValue;
   }
   display.value = output;
 };
-buttons.forEach((button) => {
+
+buttons1.forEach((button) => {
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
